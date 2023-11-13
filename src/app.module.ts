783e14295a1441981fe238736326f3c4649b202d
Bima Aria Radjasa <bima.aria.radjasa@gmail.com>
2023-11-13 07:57:32 +0700

    remove PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ContactModule],
+  imports: [ContactModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
